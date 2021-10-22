0c03cdf690279c8590ca1fdbef6b59edb5cc72a3
Lucas Silva <martssilvapro@gmail.com>
2021-10-22 11:33:49 -0300

    initial timePicker config done

--- src/components/TimePicker.jsx
+++ src/components/TimePicker.jsx
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TimePicker = () => {
+
+  const [newTimer, setNewTimer] = useState({
+    focus: 25,
+    rest: 5,
+    type: 'Focus',
+  });
+
+  const handleChange = ({target: { value, name }}) => {
+    switch (name) {
+      case 'focus':
+        setNewTimer({
+          ...newTimer,
+          focus: parseInt(value)
+        })
+        break;
+      case 'rest':
+        setNewTimer({
+          ...newTimer,
+          rest: parseInt(value)
+        })
+        break;
+    default:
+        break;
+    }
+  }
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+  }
+
+  const { focus, rest } = newTimer;
   return (
     <section className="form-section">
-      <form noValidate>
+      <form noValidate onSubmit={ handleSubmit }>
         <div className="config-inputs-container">
           <label>Focus</label>
-          <input className="config-input" />
+          <input className="config-input" name="focus" value={ focus } onChange={ handleChange } />
           <label>Rest</label>
-          <input className="config-input" />
+          <input className="config-input" name="rest" value={ rest } onChange={ handleChange } />
         </div>
         <button type="submit">Set Timer</button>
       </form>
