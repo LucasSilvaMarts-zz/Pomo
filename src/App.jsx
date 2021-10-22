49102156f4b0e70c656ea2a3afa5ed59bbecf520
Lucas Silva <martssilvapro@gmail.com>
2021-10-22 10:46:00 -0300

    Set initial timer config and styles + create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Timer from './components/Timer';
 
 function App() {
   return (
     <main>
       <h1>Pomo</h1>
       <p>Fique focado, seja feliz!</p>
+      <Timer />
     </main>
   );
 }
---
+++ src/components/Timer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
+
+const Timer = () => {
+  return (
+    <CountdownCircleTimer
+      duration={60}
+      colors={[
+          ['#00BECC', 0.33],
+          ['#00BECC', 0.33],
+          ['#00BECC', 0.33],
+        ]}
+        strokeWidth={6}
+        size={225}
+        trailColor="#444444"
+    >
+
+    </CountdownCircleTimer>
+  )
+}
+
+export default Timer
